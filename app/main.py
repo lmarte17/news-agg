f76abd2105e778dc6c5b52086896ca457c645c8c
lmarte17 <lmarte17@gmail.com>
2024-07-01 19:37:01 -0400

    fixed module import

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from scraper import scrape_articles
+from .scraper import scrape_articles
 
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
