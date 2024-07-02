460fd9c3a20b5f2cbdd8ddc8ba6be936495e3a6a
lmarte17 <lmarte17@gmail.com>
2024-07-02 10:32:00 -0400

    Updated some functionality

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from .scraper import scrape_articles
 
@@ -10,3 +10,8 @@ templates = Jinja2Templates(directory="app/templates")
 async def read_root(request: Request):
     articles = scrape_articles()
     return templates.TemplateResponse("index.html", {"request": request, "articles": articles})
+
+@app.get("/articles", response_class=JSONResponse)
+async def get_articles():
+    articles = scrape_articles()
+    return JSONResponse(content=articles)
