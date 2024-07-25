c8b3f4bf9aae8ae80f18ac5af94c723e575bf8bd
lmarte17 <lmarte17@gmail.com>
2024-07-25 17:08:48 -0400

    Settings change

--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./news_aggregator.db")
     NEWS_SOURCE_URL: str = "https://lithosgraphein.com/"
     LOG_FILE: str = "logs/news_aggregator.log"
+    HEROKU_API_KEY: str = os.getenv("HEROKU_API_KEY")
 
     class Config:
         env_file = ".env"
